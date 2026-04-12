ed8c1c556ac20deab4e06ca066e03e30c4f48965
Mark Wagh <markw@MacBook-Air.local>
2026-04-12 19:58:24 -0400

    bug fixed

--- __init__.py
+++ __init__.py
@@ -1,18 +1,13 @@
 """maze_solver — Intelligent Maze-Solving Agent"""
-from maze_solver.core.maze            import Maze
-from maze_solver.core.result          import SearchResult
-from maze_solver.algorithms.bfs       import bfs
-from maze_solver.algorithms.dfs       import dfs
-from maze_solver.algorithms.ucs       import ucs
-from maze_solver.algorithms.astar     import astar
-from maze_solver.utils.heuristics     import manhattan, euclidean
-from maze_solver.utils.comparison     import run_all, print_comparison
-from maze_solver.utils.visualizer     import print_result, plot_comparison
+from algorithms.bfs import bfs
+from algorithms.dfs import dfs
+from algorithms.ucs import ucs
+from algorithms.astar import astar
+from utils.heuristics import manhattan_distance, euclidean_distance
+from utils.evaluator import SearchResult
 
 __all__ = [
-    "Maze", "SearchResult",
     "bfs", "dfs", "ucs", "astar",
-    "manhattan", "euclidean",
-    "run_all", "print_comparison",
-    "print_result", "plot_comparison",
+    "manhattan_distance", "euclidean_distance",
+    "SearchResult",
 ]
